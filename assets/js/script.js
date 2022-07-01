71648df183878b197cebc4c2d58ba9f279042a1a
victor gonzalez <v.gonzalezgomez.vg@gmail.com>
2022-07-01 18:47:09 -0400

    dev user input for colors, amount and price, also dev total price depending of user amount

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let button = document.querySelector("#get-total");
+button.addEventListener("click", () => {
+  let amount = document.querySelector("#user-amount");
+  let userColor = document.querySelector("#user-color");
+  let price = document.querySelector("#price")
+  document.getElementById('total').innerHTML= price.value*amount.value;
+  document.getElementById('amount').innerHTML=amount.value;
+  document.getElementById('coloruser').style.backgroundColor= userColor.value;
+})
+
+
+
+
